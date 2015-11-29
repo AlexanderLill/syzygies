4cb70d5b1676dec5150dd69655948e8a4d6daa3b
Alexander Lill <AlexanderLill.AL@googlemail.com>
2015-11-29 17:38:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Info #
+
+tl;dr:
+Execute the following commands (assuming that the file "wordsEn.txt" is in the same directory as the executable) and replace execname with one of the following executable files:
+- syzygies_darwin_386 for x86
+- syzygies_darwin_amd64 for x86-64
+
+./execName -s pen -d paper
+./execName -s acorn -d oak
+./execName -s autumn -d winter
+./execName -s earthquake -d tsunami
+./execName -s starship -d enterprise
+
+
+# More Info #
+
+The program has the following command line parameters:
+
+  -f string
+    	Path to the file that contains ALL the words (default "./wordsEn.txt")
+
+  -s string
+    	The word that starts the chain
+
+  -d string
+    	The word that is the destination of the chain
+
+  -v	Verbose output
+  -vv  	Debugging output
+
+
+# Building Go Programs
+
+If interested this program can easily be built by yourself:
+
+- Download go ([https://golang.org/dl/])
+- Open Commandline, navigate to the folder including "syzygies.go", and type "go build"
+- Check if the executable "syzygies" has been created, and run it.
---
+++ syzygies.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+
+	// Commandline option parsing
+	"flag"
+	"log"
+	"bufio"
+	"os"
+)
+
+var fileWithWords *string = flag.String("f", "./wordsEn.txt", "Path to the file that contains ALL the words")
+var startWord *string = flag.String("s", "", "The word that starts the chain")
+var destinationWord *string = flag.String("d", "", "The word that is the destination of the chain")
+var isVerbose *bool = flag.Bool("v", false, "Verbose output")
+var isDebug *bool = flag.Bool("vv", false, "Debugging output")
+
+type WordWithPath struct {
+	word string
+	path []string
+}
+
+
+func main() {
+
+	// Parse the command line options
+	flag.Parse()
+
+	Verbose("isVerbose:", *isVerbose)
+	Verbose("isDebug:", *isDebug)
+	Verbose("fileWithWords:", *fileWithWords)
+	Verbose("startWord:", *startWord)
+	Verbose("destinationWord:", *destinationWord)
+
+	Verbose("##### Loading words from file " + *fileWithWords)
+	words := LoadWordListFromFileAndCheckForWords(*fileWithWords, *startWord, *destinationWord)
+	Debug(words)
+
+	Verbose("##### Creating prepared lists from huge word list")
+	wordMap := SplitWordListIntoSubsets(words)
+	Debug(wordMap)
+
+	Verbose("##### Searching path from '", *startWord, "' to word '", *destinationWord, "'...")
+	startWordWithPath := WordWithPath{*startWord, []string{}}
+	frontier := []WordWithPath{startWordWithPath}
+	explored := make([]WordWithPath, 0)
+	Verbose(FindPath(&wordMap, frontier, explored, *destinationWord))
+	
+
+	//Verbose(GetPossibleWords(&wordMap, "Lab", "Fus"))
+	//Verbose(ListIncludesWord(GetPossibleWords(&wordMap, "ab", "us"), "abacus"))
+}
+
+func LoadWordListFromFileAndCheckForWords(filename string, startWord string, destinationWord string) (words []string) {
+
+	var foundStartWord bool = false
+	var foundEndWord bool = false
+
+    file, err := os.Open(filename)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+    	word := scanner.Text()
+    	if word == startWord {
+    		foundStartWord = true
+    	}
+    	if word == destinationWord {
+    		foundEndWord = true
+    	}
+    	words = append(words, word)
+    }
+
+    if err := scanner.Err(); err != nil {
+        log.Fatal(err)
+    }
+
+	Verbose("Loaded ", len(words), " words!")
+
+	if !foundStartWord || !foundEndWord {
+		Error("D'oh! Could not find start- or destinationWord in word list!11!!")
+	}
+
+    return
+}
+
+func SplitWordListIntoSubsets(words []string) (wordMap map[string][]string) {
+
+	wordMap = make(map[string][]string, 0)
+
+	for _, word := range words {
+		firstTwoLetters := GetFirstTwoLetters(word)
+		lastTwoLetters := GetLastTwoLetters(word)
+
+		Verbose("Word=", word, " - ", firstTwoLetters, "...", lastTwoLetters)
+
+		wordMap["F" + firstTwoLetters] = append(wordMap["F" + firstTwoLetters], word)
+		wordMap["L" + lastTwoLetters] = append(wordMap["L" + lastTwoLetters], word)
+	}
+	return
+}
+
+func GetFirstTwoLetters(word string) (letters string) {
+	if len(word)>=2 {
+		letters = word[0:2]
+	} else {
+		letters = word
+	}
+	return
+}
+
+func GetLastTwoLetters(word string) (letters string) {
+	if len(word)>=2 {
+		letters = word[len(word)-2:]
+	} else {
+		letters = word
+	}
+	return
+}
+
+func FindPath(wordMap *map[string][]string, frontier []WordWithPath, explored []WordWithPath, destinationWord string) bool {
+
+	for len(frontier) > 0 {
+
+		currentWord := frontier[0]
+		frontier = frontier[1:]
+
+		if ListIncludesWord(explored, currentWord.word) {
+			Verbose("Already in explored, NEXT: ", currentWord)
+			continue
+		}
+
+		explored = append(explored, currentWord)
+
+		//Print("FRONTIERSIZE: ", len(frontier), "EXPLOREDSIZE: ", len(explored))
+
+		firstTwoLetters := GetFirstTwoLetters(currentWord.word)
+		//Verbose("firstTwoLetters of", currentWord.word, "=", firstTwoLetters)
+
+		lastTwoLetters := GetLastTwoLetters(currentWord.word)
+		//Verbose("lastTwoLetters of", currentWord.word, "=", lastTwoLetters)
+
+		possibleWords := GetPossibleWords(wordMap, "L" + firstTwoLetters, "F" + lastTwoLetters)
+		//Print("POSSIBLEWORDS: ", len(possibleWords))
+
+		for _, word := range possibleWords {
+
+			//Verbose("Examining word ", word)
+
+			if word == destinationWord {
+				Debug("FOUND! ", word, currentWord)
+				PrintWordChain(word, currentWord)
+				return true
+			}
+
+			/*if ListIncludesWord(frontier, word) {
+				//Verbose("Already in frontier: ", word)
+				continue
+			}
+
+			if ListIncludesWord(explored, word) {
+				//Verbose("Already in explored: ", word)
+				continue
+			}*/
+
+			//Verbose("Adding ", word, "to the frontier...")
+			newWord := WordWithPath{}
+			newWord.word = word
+			newWord.path = append(currentWord.path, currentWord.word)
+
+			frontier = append(frontier, newWord)
+
+		}
+
+	}
+
+	//Print("FRONTIER IS EMPTY")
+	return false
+}
+
+func PrintWordChain(lastWord string, parentWord WordWithPath) {
+	Debug(len(parentWord.path))
+
+	for _, value := range parentWord.path {
+		Print(value)
+	}
+	Print(parentWord.word)
+	Print(lastWord)
+	Print("Chainsize:", 2 + len(parentWord.path))
+}
+
+func ListIncludesWord(list []WordWithPath, word string) bool {
+	for _, w := range list {
+		if w.word == word {
+			return true
+		}
+	}
+	return false
+}
+
+func GetPossibleWords(wordMap *map[string][]string, firstTwoLetters string, lastTwoLetters string) (words []string) {
+	return append((*wordMap)[firstTwoLetters], (*wordMap)[lastTwoLetters]...)
+}
+
+func Print(args ...interface{}) {
+	log.Print(args)
+}
+
+func Verbose(args ...interface{}) {
+	if *isVerbose || *isDebug {
+		log.Print(args)
+	}
+}
+
+func Debug(args ...interface{}) {
+	if *isDebug {
+		log.Printf("# %v", args)
+	}
+}
+
+func Error(args ...interface{}) {
+	log.Fatalf("ERROR %v", args)
+}
